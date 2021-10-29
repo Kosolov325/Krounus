4fcec9f303c42958a23cb385fda5eea4b2f6f91c
Kosolov325 <58604882+Kosolov325@users.noreply.github.com>
2021-10-29 08:44:26 -0300

    Buld script
    making it functional on linux SO

--- build_module.sh
+++ build_module.sh
@@ -1,8 +1,3 @@
-if [ -d "Persistent Kingdoms" ]; then
-  echo "cleaning previous build"
-  rm -R Persistent\ Kingdoms/
-fi
-mkdir Persistent\ Kingdoms/
 python2 -tt build_module.py -d
 rm *.pyc
 echo "build complete"
