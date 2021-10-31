a0208df5307f90814df8e612e228ca3ac7b7fee7
MidZ <mondechju@wp.pl>
2021-10-31 16:16:30 +0100

    fixed tomfoolery

--- module_mission_templates.py
+++ module_mission_templates.py
@@ -414,14 +414,6 @@ agent_hit = (ti_on_agent_hit, 0, 0, [], # server: apply extra scripted effects f
     (store_trigger_param_3, ":damage_dealt"),
 
     (try_begin),
-    (position_set_x, pos1 ,-100000),
-	  (position_set_y, pos1 ,-100000),
-	  (position_set_z, pos1 , 0),
-	  (agent_set_position,":attacker_agent_id" , pos1 ),
-    (try_end),
-    
-    
-    (try_begin),
       (neg|is_between, reg0, scripted_items_begin, scripted_items_end),
       (neq, reg0, "itm_baton"),
       (agent_is_human, ":attacker_agent_id"),
