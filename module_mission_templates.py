04f8970235217c22702cdf95111ce700220522ea
MidZ <mondechju@wp.pl>
2021-10-31 13:46:53 +0100

    fixed invalid syntax
    trying to war with loops

--- module_mission_templates.py
+++ module_mission_templates.py
@@ -213,9 +213,10 @@ after_mission_start_setup = (ti_after_mission_start, 0, 0, [], # spawn and move 
     ## PK.js SCRIPTS END ##
 
     #MidZ war between facs
-    (try_for_range,":fac1", castle_factions_begin, factions_end - 1),
+    (try_for_range,":fac1", castle_factions_begin, factions_end),
 	    (try_for_range, ":fac2", ":fac1", factions_end),
         (neq, ":fac1", ":fac2"),
+        (neq, "fac1" "fac_8"),
 		    (call_script, "script_cf_faction_change_relation", "fac1", "fac2", 0), 
 		  (try_end),
 	  (try_end),
